8f3cb44e8e2c3f100ce29f6dc890b507f4935117
Peter A. Weinwurm <jrweinwurm@hotmail.com>
2024-09-03 13:08:50 -0400

    Another Crazy Error

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 using TelerikUI.Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TelerikUI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +19,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddTelerikBlazor();
-builder.Services.AddDbContext();
+builder.Services.AddDbContext<AppDBContext>();
 
 var app = builder.Build();
 
